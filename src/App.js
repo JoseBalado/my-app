4aadd03f0865f9d0cb84081526c549f3f163508e
Jose Balado <JoseBalado@users.noreply.github.com>
2022-06-12 00:37:15 +0200

    Strange commit.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import Chat from './Chat/Chat';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div style={{ margin: '0 30%' }}>
-      <Chat />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
