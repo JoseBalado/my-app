2949524217bae0e20f2656eff5719b6b498c3c16
Jose Balado <JoseBalado@users.noreply.github.com>
2022-06-15 01:09:52 +0200

    Print grid.

--- src/Chat/Chart.js
+++ src/Chat/Chart.js
@@ -1,5 +1,35 @@
 import React, { useState, useEffect, useRef } from 'react';
 
+// vertical lines
+const gridV = ctx => {
+    var canvasWidth = ctx.canvas.width;
+    var canvasHight = ctx.canvas.height;
+
+    for(var i = 0; i <= 10; i++){
+        ctx.strokeStyle = 'rgba(29, 210, 175,0.3)';
+        ctx.lineWidth = 1;
+        ctx.moveTo(i * canvasWidth / 10, 0);
+        ctx.lineTo(i * canvasWidth / 10, canvasHight);
+    }
+
+    ctx.stroke();
+}
+
+//horizontal lines
+const gridH = ctx => {
+    var canvasWidth = ctx.canvas.width;
+    var canvasHight = ctx.canvas.height;
+
+    for(var i = 0; i <= 10; i++){
+        ctx.strokeStyle = 'rgba(29, 210, 175,0.3)';
+        ctx.lineWidth = 1;
+        ctx.moveTo(0, i * canvasHight / 10);
+        ctx.lineTo(canvasWidth, i * canvasHight / 10);
+    }
+
+    ctx.stroke();
+}
+
 
 const Chart = (props) => {
     console.log(props)
@@ -23,12 +53,15 @@ const Chart = (props) => {
             seconds.current.appendChild(second);
         }
 
-        for(let i = 10; i >= 1; i--) {
+        for (let i = 10; i >= 1; i--) {
             var value = document.createElement('span');
             var text_percentage = document.createTextNode(`${i * 10}`)
             value.appendChild(text_percentage);
             percentage.current.appendChild(value);
         }
+
+        gridV(ctx);
+        gridH(ctx);
     });
 
     return (
